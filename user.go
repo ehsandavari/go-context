9333f1721b3ac5499953af7091dfdb90b7efdcae
ehsandavari <ehsandavari.ir@gmail.com>
2023-09-03 01:25:06 +0330

    user

--- user.go
+++ user.go
@@ -5,8 +5,11 @@ import (
 )
 
 type User struct {
-	id          uuid.UUID
-	phoneNumber string
+	id                  uuid.UUID
+	email               string
+	emailVerified       bool
+	phoneNumber         string
+	phoneNumberVerified bool
 }
 
 func (r *User) Id() uuid.UUID {
@@ -17,10 +20,34 @@ func (r *User) SetId(id uuid.UUID) {
 	r.id = id
 }
 
+func (r *User) Email() string {
+	return r.email
+}
+
+func (r *User) SetEmail(email string) {
+	r.email = email
+}
+
+func (r *User) EmailVerified() bool {
+	return r.emailVerified
+}
+
+func (r *User) SetEmailVerified(emailVerified bool) {
+	r.emailVerified = emailVerified
+}
+
 func (r *User) PhoneNumber() string {
 	return r.phoneNumber
 }
 
 func (r *User) SetPhoneNumber(phoneNumber string) {
 	r.phoneNumber = phoneNumber
+}
+
+func (r *User) PhoneNumberVerified() bool {
+	return r.phoneNumberVerified
+}
+
+func (r *User) SetPhoneNumberVerified(phoneNumberVerified bool) {
+	r.phoneNumberVerified = phoneNumberVerified
 }
